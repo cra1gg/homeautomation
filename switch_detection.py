bee3ab599b22a868f7425c87a270d9e3ecd5fac8
Craig D'Souza <craigg.dsouza@mail.utoronto.ca>
2020-06-21 14:42:40 -0400

    Add adb support

--- switch_detection.py
+++ switch_detection.py
@@ -2,24 +2,85 @@ import pywemo
 import time
 import os
 import subprocess
+from pyvesync import VeSync
+
+manager = VeSync("cfdt123@gmail.com", "Marlenne123")
+manager.login()
+manager.update()
+
+my_switch = manager.outlets[0]
+
+address = "192.168.1.177"
+port = pywemo.ouimeaux_device.probe_wemo(address)
+url = 'http://%s:%i/setup.xml' % (address, port)
+
+on = True
+device = pywemo.discovery.device_from_description(url, None)
+if device.get_state() == 1:
+    my_switch.turn_on()
+    cmdCommand = "nircmd.exe monitor on"   #specify your cmd command
+    process = subprocess.Popen(cmdCommand.split(), stdout=subprocess.PIPE)
+    on = True
+    cmdCommand = "adb\\adb.exe shell service call power 12"   #specify your cmd command
+    process = subprocess.Popen(cmdCommand.split(), stdout=subprocess.PIPE)
+    output, error = process.communicate()
+    print(output.decode("utf-8") )
+    if output == "Result: Parcel(00000000 00000000   '........')\r\n":
+                cmdCommand = "adb\\adb.exe shell input keyevent 26"   #specify your cmd command
+                process = subprocess.Popen(cmdCommand.split(), stdout=subprocess.PIPE)
+                output, error = process.communicate()
+    
+else:
+    on = False
+    my_switch.turn_off()
+    print("Turning monitor off")
+    cmdCommand = "nircmd.exe monitor off"   #specify your cmd command
+    process = subprocess.Popen(cmdCommand.split(), stdout=subprocess.PIPE)
+    output, error = process.communicate()
+    cmdCommand = "adb\\adb.exe shell service call power 12"   #specify your cmd command
+    process = subprocess.Popen(cmdCommand.split(), stdout=subprocess.PIPE)
+    output, error = process.communicate()
+    print(output.decode("utf-8") )
+    if output == "Result: Parcel(00000000 00000001   '........')\r\n":
+                cmdCommand = "adb\\adb.exe shell input keyevent 26"   #specify your cmd command
+                process = subprocess.Popen(cmdCommand.split(), stdout=subprocess.PIPE)
+                output, error = process.communicate()
 
 while True:
-    address = "192.168.1.177"
-    port = pywemo.ouimeaux_device.probe_wemo(address)
-    url = 'http://%s:%i/setup.xml' % (address, port)
     device = pywemo.discovery.device_from_description(url, None)
     if device.get_state() == 1:
-        print("Turning monitor on")
-        cmdCommand = "nircmd.exe monitor on"   #specify your cmd command
-        process = subprocess.Popen(cmdCommand.split(), stdout=subprocess.PIPE)
-        output, error = process.communicate()
-        print(output)
+        if not on:
+            on = True
+            my_switch.turn_on()
+            print("Turning monitor on")
+            cmdCommand = "nircmd.exe monitor on"   #specify your cmd command
+            process = subprocess.Popen(cmdCommand.split(), stdout=subprocess.PIPE)
+            output, error = process.communicate()
+            cmdCommand = "adb\\adb.exe shell service call power 12"   #specify your cmd command
+            process = subprocess.Popen(cmdCommand.split(), stdout=subprocess.PIPE)
+            output, error = process.communicate()
+            print(output.decode("utf-8"))
+            if output == "Result: Parcel(00000000 00000001   '........')\r\n":
+                cmdCommand = "adb\\adb.exe shell input keyevent 26"   #specify your cmd command
+                process = subprocess.Popen(cmdCommand.split(), stdout=subprocess.PIPE)
+                output, error = process.communicate()
+
    
     else:
-        print("Turning monitor off")
-        cmdCommand = "nircmd.exe monitor off"   #specify your cmd command
-        process = subprocess.Popen(cmdCommand.split(), stdout=subprocess.PIPE)
-        output, error = process.communicate()
-        print(output)
+        if on:
+            on = False
+            my_switch.turn_off()
+            print("Turning monitor off")
+            cmdCommand = "nircmd.exe monitor off"   #specify your cmd command
+            process = subprocess.Popen(cmdCommand.split(), stdout=subprocess.PIPE)
+            output, error = process.communicate()
+            cmdCommand = "adb\\adb.exe shell service call power 12"   #specify your cmd command
+            process = subprocess.Popen(cmdCommand.split(), stdout=subprocess.PIPE)
+            output, error = process.communicate()
+            print(output.decode("utf-8"))
+            if output == "Result: Parcel(00000000 00000000   '........')\r\n":
+                cmdCommand = "adb\\adb.exe shell input keyevent 26"   #specify your cmd command
+                process = subprocess.Popen(cmdCommand.split(), stdout=subprocess.PIPE)
+                output, error = process.communicate()
     print(device.get_state())
     
