0cd7a1323007fc4ae0ec1b449068cc1553f247c9
Craig D'Souza <craigg.dsouza@mail.utoronto.ca>
2020-06-28 00:51:27 -0400

    Fix adb screen-on detection and control

--- switch_detection.py
+++ switch_detection.py
@@ -14,12 +14,12 @@ my_switch = manager.outlets[0]
 address = "192.168.1.177"
 port = pywemo.ouimeaux_device.probe_wemo(address)
 url = 'http://%s:%i/setup.xml' % (address, port)
-pyautogui.FAILSAFe = False
+pyautogui.FAILSAFE = False
 on = True
 device = pywemo.discovery.device_from_description(url, None)
 if device.get_state() == 1:
     my_switch.turn_on()
-    pyautogui.click(100, 100)
+    pyautogui.click()
     cmdCommand = "nircmd.exe monitor on"   #specify your cmd command
     process = subprocess.Popen(cmdCommand.split(), stdout=subprocess.PIPE)
     on = True
@@ -27,10 +27,11 @@ if device.get_state() == 1:
     process = subprocess.Popen(cmdCommand.split(), stdout=subprocess.PIPE)
     output, error = process.communicate()
     print(output.decode("utf-8") )
-    if output == "Result: Parcel(00000000 00000000   '........')\r\n":
-                cmdCommand = "adb\\adb.exe shell input keyevent 26"   #specify your cmd command
-                process = subprocess.Popen(cmdCommand.split(), stdout=subprocess.PIPE)
-                output, error = process.communicate()
+    if output.decode("utf-8") == "Result: Parcel(00000000 00000000   '........')\r\n":
+        print("inside if!")
+        cmdCommand = "adb\\adb.exe shell input keyevent 26"   #specify your cmd command
+        process = subprocess.Popen(cmdCommand.split(), stdout=subprocess.PIPE)
+        output, error = process.communicate()
     
 else:
     on = False
@@ -43,17 +44,18 @@ else:
     process = subprocess.Popen(cmdCommand.split(), stdout=subprocess.PIPE)
     output, error = process.communicate()
     print(output.decode("utf-8") )
-    if output == "Result: Parcel(00000000 00000001   '........')\r\n":
-                cmdCommand = "adb\\adb.exe shell input keyevent 26"   #specify your cmd command
-                process = subprocess.Popen(cmdCommand.split(), stdout=subprocess.PIPE)
-                output, error = process.communicate()
+    if output.decode("utf-8") == "Result: Parcel(00000000 00000001   '........')\r\n":
+        print("inside if!")
+        cmdCommand = "adb\\adb.exe shell input keyevent 26"   #specify your cmd command
+        process = subprocess.Popen(cmdCommand.split(), stdout=subprocess.PIPE)
+        output, error = process.communicate()
 
 while True:
     device = pywemo.discovery.device_from_description(url, None)
     if device.get_state() == 1:
         if not on:
             on = True
-            pyautogui.click(100, 100)
+            pyautogui.click()
             my_switch.turn_on()
             print("Turning monitor on")
             cmdCommand = "nircmd.exe monitor on"   #specify your cmd command
@@ -63,7 +65,8 @@ while True:
             process = subprocess.Popen(cmdCommand.split(), stdout=subprocess.PIPE)
             output, error = process.communicate()
             print(output.decode("utf-8"))
-            if output == "Result: Parcel(00000000 00000001   '........')\r\n":
+            if output.decode("utf-8") == "Result: Parcel(00000000 00000000   '........')\r\n":
+                print("inside if!")
                 cmdCommand = "adb\\adb.exe shell input keyevent 26"   #specify your cmd command
                 process = subprocess.Popen(cmdCommand.split(), stdout=subprocess.PIPE)
                 output, error = process.communicate()
@@ -81,7 +84,8 @@ while True:
             process = subprocess.Popen(cmdCommand.split(), stdout=subprocess.PIPE)
             output, error = process.communicate()
             print(output.decode("utf-8"))
-            if output == "Result: Parcel(00000000 00000000   '........')\r\n":
+            if output.decode("utf-8") == "Result: Parcel(00000000 00000001   '........')\r\n":
+                print("inside if!")
                 cmdCommand = "adb\\adb.exe shell input keyevent 26"   #specify your cmd command
                 process = subprocess.Popen(cmdCommand.split(), stdout=subprocess.PIPE)
                 output, error = process.communicate()
